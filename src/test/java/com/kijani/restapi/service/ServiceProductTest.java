3263163dd83b45317ce91d54f97a8af4b58346a1
jeppe-t <jeppethylstrup@yahoo.dk>
2022-05-04 16:54:37 +0200

    Refactoring test..

--- src/test/java/com/kijani/restapi/service/ServiceProductTest.java
+++ src/test/java/com/kijani/restapi/service/ServiceProductTest.java
@@ -2,11 +2,8 @@ package com.kijani.restapi.service;
 
 import com.kijani.restapi.model.Product;
 import com.kijani.restapi.repository.ProductRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
