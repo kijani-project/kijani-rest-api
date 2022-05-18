b782585b2863b75a372e7b0c5803b8534836dc0e
Mathias Reker <mathias@reker.dk>
2022-05-18 09:58:38 +0200

    postman

---
+++ src/main/java/com/kijani/restapi/data/DataMapper.java
@@ -0,0 +1,12 @@
+package com.kijani.restapi.data;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataMapper implements CommandLineRunner {
+
+  public void run(String... args) throws Exception {
+
+  }
+}
