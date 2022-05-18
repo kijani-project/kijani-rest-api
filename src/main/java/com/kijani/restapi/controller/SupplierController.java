70682865790f5d4f62e555f0a7acde5f7d3e66a0
Geofery <Geofery@users.noreply.github.com>
2022-05-18 12:31:47 +0000

    Google Java Format

--- src/main/java/com/kijani/restapi/controller/SupplierController.java
+++ src/main/java/com/kijani/restapi/controller/SupplierController.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
